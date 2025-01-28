2263e5d6d004c04c1039c9488dadf7123baad3eb
dsvasudev19 <vasudevds1729@gmail.com>
2025-01-28 21:39:34 +0530

    up

--- projexpert/src/axiosIntance.ts
+++ projexpert/src/axiosIntance.ts
@@ -19,52 +19,52 @@ axiosInstance.interceptors.request.use(
   }
 );
 
-axiosInstance.interceptors.response.use(
-  (response) => {
-    // Return the response if it's successful
-    return response;
-  },
-  async (error) => {
-    const originalRequest = error.config;
+// axiosInstance.interceptors.response.use(
+//   (response) => {
+//     // Return the response if it's successful
+//     return response;
+//   },
+//   async (error) => {
+//     const originalRequest = error.config;
 
-    if (error.response && error.response.status === 401) {
-      let retries: any = localStorage.getItem("request_retries");
-      retries = retries ? parseInt(retries, 10) : 0;
+//     if (error.response && error.response.status === 401) {
+//       let retries: any = localStorage.getItem("request_retries");
+//       retries = retries ? parseInt(retries, 10) : 0;
 
-      if (!retries) {
-        try {
-          localStorage.setItem("request_retries", "1");
+//       if (!retries) {
+//         try {
+//           localStorage.setItem("request_retries", "1");
 
-          const tokenFound = localStorage.getItem("refreshToken")
+//           const tokenFound = localStorage.getItem("refreshToken")
 
-          const refreshResponse = await axiosInstance.post("/auth/refresh-token?token=" + tokenFound);
+//           const refreshResponse = await axiosInstance.post("/auth/refresh-token?token=" + tokenFound);
 
-          if (refreshResponse.status === 200) {
-            const newToken = refreshResponse.data.token;
+//           if (refreshResponse.status === 200) {
+//             const newToken = refreshResponse.data.token;
 
-            localStorage.setItem("__auth", newToken);
-            originalRequest.headers.Authorization = `Bearer ${newToken}`;
-            localStorage.setItem("request_retries", "0");
-          } else {
-            // If status is not 200, redirect to login
-            localStorage.removeItem("__auth");
-            window.location.href = "/auth/login";
-          }
+//             localStorage.setItem("__auth", newToken);
+//             originalRequest.headers.Authorization = `Bearer ${newToken}`;
+//             localStorage.setItem("request_retries", "0");
+//           } else {
+//             // If status is not 200, redirect to login
+//             localStorage.removeItem("__auth");
+//             window.location.href = "/auth/login";
+//           }
 
-          return axiosInstance(originalRequest);
+//           return axiosInstance(originalRequest);
 
-        } catch (refreshError) {
+//         } catch (refreshError) {
 
-          console.error("Token refresh failed", refreshError);
-        }
-      }
+//           console.error("Token refresh failed", refreshError);
+//         }
+//       }
 
-      // If refresh token fails or retries are greater than 0, redirect to login
-      localStorage.removeItem("__auth");
-      window.location.href = "/auth/login";
-    }
+//       // If refresh token fails or retries are greater than 0, redirect to login
+//       localStorage.removeItem("__auth");
+//       window.location.href = "/auth/login";
+//     }
 
-    return Promise.reject(error);
-  }
-);
+//     return Promise.reject(error);
+//   }
+// );
 
